276c1d1ca1389f878c3d402a4b07af417ab6be4c
kkupalro <34331235+kkupalro@users.noreply.github.com>
2019-10-10 00:18:53 +0900

    Create 주식가격.java

---
+++ 프로그래머스/LV2/주식가격.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        for(int i = 0; i < answer.length - 1; i++)
+        {
+            int time = 0;
+            for(int j = i+1; j < answer.length; j++)
+            {
+                time++;
+                if(prices[i] > prices[j]) break;
+            }
+            answer[i] = time;
+        }
+        return answer;
+    }
+}
