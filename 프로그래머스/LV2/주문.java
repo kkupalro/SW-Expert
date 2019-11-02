e8a0519b71ac8b6a31f2a0e969585e010c0e5b05
kkupalro <34331235+kkupalro@users.noreply.github.com>
2019-11-02 13:19:31 +0900

    Update 주문.java

--- 프로그래머스/LV2/주문.java
+++ 프로그래머스/LV2/주문.java
@@ -6,12 +6,13 @@ public class Main {
 	static int result = Integer.MAX_VALUE;
 	static int input[] = {1,2,3,1,2,2,3};
 	public static void main(String[] args) throws IOException {
-		// BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		for(int i = 1; i < input.length; i++)
 		{
+			if(result == 1) break;
 			for(int j = i-1; j >= 0; j--)
 			{
+				if(i-j > result) break;
 				if(input[j] == input[i])
 				{
 					result = Math.min(result, i-j);
