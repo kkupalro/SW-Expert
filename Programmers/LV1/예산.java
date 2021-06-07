582067d53c0c571ccd0f6a6e942d0c789ba5383a
kkupalro <dupalronet@hanmail.net>
2021-06-07 16:08:51 +0900

    0607|Programmers|LV1

--- Programmers/LV1/예산.java
+++ Programmers/LV1/예산.java
@@ -1,27 +1,18 @@
+import java.util.Arrays;
+
 class Solution {
-  public int solution(int[] d, int budget) {
-      int answer = 0;
-      for(int i = 0; i < d.length-1; i++)
-      {
-          for(int j = i+1; j < d.length; j++)
-          {
-              if(d[i] > d[j])
-              {
-                  d[i] ^= d[j];
-                  d[j] ^= d[i];
-                  d[i] ^= d[j];
-              }
-          }
+  static int solution(int[] d, int budget) {
+    int answer = 0;
+    Arrays.sort(d);
+    for (int i = 0; i < d.length; i++) {
+      if (budget >= d[i]) {
+        budget -= d[i];
+        answer++;
+      } else {
+        break;
       }
-      for(int i = 0; i < d.length; i++)
-      {
-          if(budget >= d[i]) 
-          {
-              budget -= d[i];
-              answer++;
-          }
-          else break;
-      }
-      return answer;
+    }
+    return answer;
   }
+
 }
