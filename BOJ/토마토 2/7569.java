42fd47911151f421f04dda80e63321a9f1fd7ca1
kkupalro <dupalronet@hanmail.net>
2021-06-26 16:17:39 +0900

    0626|BOJ|문제데이터변경|7569

--- BOJ/토마토 2/7569.java
+++ BOJ/토마토 2/7569.java
@@ -1,10 +1,13 @@
-package TEST;
+package DataStructure;
 
 import java.io.*;
 import java.util.*;
 
 class node {
-	int y; int x; int h;
+	int y;
+	int x;
+	int h;
+
 	node(int y, int x, int h) {
 		this.y = y;
 		this.x = x;
@@ -13,108 +16,87 @@ class node {
 }
 
 public class Main {
-	static final int dy[] = {-1, 0, 1, 0}; // ^ > v <
-	static final int dx[] = { 0, 1, 0,-1};
-	static int matrix[][][];
-	static int Y, X, H, result;
-	static Queue<node> q = new LinkedList<node>();
-	static void solve() {
-		int ny = 0; int nx = 0; int size = 0;
-		while(!q.isEmpty())
-		{
+	static final int dy[] = { -1, 0, 1, 0 };
+	static final int dx[] = { 0, 1, 0, -1 };
+	static final int dh[] = { -1, 1 };
+	static int M, N, H, result;
+	static int[][][] matrix;
+	static Queue<node> q;
+	static int cnt;
+
+	static void bfs() {
+		int ny = 0;
+		int nx = 0;
+		int size = 0;
+		while (!q.isEmpty()) {
 			size = q.size();
-			for(int i = 0; i < size; i++)
-			{
+			result++;
+			for (int i = 0; i < size; i++) {
 				node n = q.poll();
 				int y = n.y;
 				int x = n.x;
 				int h = n.h;
-				for(int l = 0; l < 4; l++)
-				{
-					ny = y + dy[l];
-					nx = x + dx[l];
-					if(ny < 0 || ny > Y-1 || nx < 0 || nx > X-1) continue;
-					if(matrix[h][ny][nx] == -1 || matrix[h][ny][nx] == 1) continue;
+				for (int dir = 0; dir < 4; dir++) {
+					ny = y + dy[dir];
+					nx = x + dx[dir];
+					if (ny < 0 || ny > N - 1 || nx < 0 || nx > M - 1) continue;
+					if (matrix[h][ny][nx] == -1 || matrix[h][ny][nx] == 1) continue;
 					matrix[h][ny][nx] = 1;
+					cnt--;
 					q.offer(new node(ny, nx, h));
 				}
-				if(H == 1) break;
-				if(h == 0)
-				{
-					if(matrix[h+1][y][x] == -1 || matrix[h+1][y][x] == 1) continue;
-					matrix[h+1][y][x] = 1;
-					q.offer(new node(y, x, h+1));
-				}
-				else if(h == H-1)
-				{
-					if(matrix[h-1][y][x] == -1 || matrix[h-1][y][x] == 1) continue; 
-					matrix[h-1][y][x] = 1;
-					q.offer(new node(y, x, h-1));
-				}
-				else 
-				{
-					if(matrix[h-1][y][x] != -1 && matrix[h-1][y][x] != 1) 
-					{
-						matrix[h-1][y][x] = 1;
-						q.offer(new node(y, x, h-1));
-					}
-					if(matrix[h+1][y][x] != -1 && matrix[h+1][y][x] != 1)
-					{
-						matrix[h+1][y][x] = 1;
-						q.offer(new node(y, x, h+1));
-					}
+
+				for (int dir = 0; dir < 2; dir++) {
+					int nh = h + dh[dir];
+					if (nh < 0 || nh > H - 1) continue;
+					if (matrix[nh][y][x] == -1 || matrix[nh][y][x] == 1) continue;
+					matrix[nh][y][x] = 1;
+					cnt--;
+					q.offer(new node(y, x, nh));
 				}
 			}
-			result++;
 		}
-		result--;
 	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = null;
-		st = new StringTokenizer(br.readLine(), " ");
-		X = Integer.parseInt(st.nextToken());
-		Y = Integer.parseInt(st.nextToken());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
 		H = Integer.parseInt(st.nextToken());
-		matrix = new int[H][Y][X];
-		for(int h = 0; h < H; h++)
-		{
-			for(int i = 0; i < Y; i++)
-			{
+		matrix = new int[H][N][M];
+		q = new LinkedList<node>();
+
+		for (int k = 0; k < H; k++) {
+			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine(), " ");
-				for(int j = 0; j < X; j++)
-				{
-					matrix[h][i][j] = Integer.parseInt(st.nextToken());
-					if(matrix[h][i][j] == 1)
-					{
-						q.offer(new node(i, j, h));
+				for (int j = 0; j < M; j++) {
+					matrix[k][i][j] = Integer.parseInt(st.nextToken());
+					if (matrix[k][i][j] == 1) {
+						q.offer(new node(i, j, k));
+					}
+					if (matrix[k][i][j] == 0) {
+						cnt++;
 					}
 				}
 			}
 		}
-		if(q.isEmpty())
-		{
+
+		// 모든 토마토가 익어있는 상태
+		if (cnt == 0) {
 			result = 0;
-		}
-		else
-		{
-			solve();
-			for(int h = 0; h < H; h++)
-			{
-				for(int i = 0; i < Y; i++)
-				{
-					for(int j = 0; j < X; j++)
-					{
-						if(matrix[h][i][j] == 0)
-						{
-							result = -1;
-							break;
-						}
-					}
-				}
+		} else {
+			result = -1;
+			bfs();
+
+			// 덜 익은 토마토가있는 상태
+			if (cnt != 0) {
+				result = -1;
 			}
 		}
+
 		bw.write(result + "\n");
 		bw.flush();
 		bw.close();
