20c8e4992339e3b07229c6d14eb500006f201b5c
kkupalro <dupalronet@hanmail.net>
2021-06-28 23:29:07 +0900

    0628|BOJ|A-SA 문제집-2|1018

--- BOJ/체스판 다시 칠하기/1018.java
+++ BOJ/체스판 다시 칠하기/1018.java
@@ -1,108 +1,68 @@
-package TEST;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
-import java.io.*;
-import java.util.*;
-
+/**
+ * @author yoo
+ * @commit 0627|BOJ|A-SA 문제집-2|1018
+ *
+ */
 public class Main {
-	static StringBuilder sb = new StringBuilder();
-	static int matrix[][];
-	static int W[][] = new int[8][8];
-	static int B[][] = new int[8][8];
-	static int N, M, result;
-	static void print(int a[][]) {
-		for (int i = 0; i < a.length; i++) 
-		{
-			for (int j = 0; j < a[i].length; j++) 
-			{
-				System.out.print(a[i][j] + " ");
-			}
-			System.out.println();
-		}
-	}
+	static BufferedReader br;
+	static BufferedWriter bw;
+	static StringTokenizer st;
+	static int N, M;
+	static int result = Integer.MAX_VALUE;
+	static ArrayList<String> al = new ArrayList<String>();
+	static boolean matrix[][];
+	static boolean board[][][] = new boolean[2][8][8];
+
 	static void solve(int y, int x) {
-		int r = 0; int c = 0; int ans = 0;
-		for(int i = y; i < y+8; i++)
-		{
-			c = 0;
-			for(int j = x; j < x+8; j++)
-			{
-				if(matrix[i][j] != W[r][c])
-				{
-					ans++;
-				}
-				c++;
+		int w = 0;
+		int b = 0;
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				if (matrix[y + i][x + j] != board[0][i][j])
+					w++;
+				if (matrix[y + i][x + j] != board[1][i][j])
+					b++;
 			}
-			r++;
 		}
-		result = Math.min(result, ans);
-		ans = 0; r = 0; c = 0;
-		for(int i = y; i < y+8; i++)
-		{
-			c = 0;
-			for(int j = x; j < x+8; j++)
-			{
-				if(matrix[i][j] != B[r][c])
-				{
-					ans++;
-				}
-				c++;
-			}
-			r++;
-		}
-		result = Math.min(result, ans);
+		result = Math.min(result, Math.min(w, b));
 	}
-	
+
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = null;
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		st = new StringTokenizer(br.readLine(), " ");
-		result = Integer.MAX_VALUE;
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		matrix = new int[N][M];
-		boolean flag = false;
-		for(int i = 0; i < 8; i++)
-		{
-			for(int j = 0; j < 8; j++)
-			{
-				flag ^= true;
-				if(flag)
-				{
-					W[i][j] = 0;
-					B[i][j] = 1;
-				}
-				else
-				{
-					W[i][j] = 1;
-					B[i][j] = 0;
-				}
-			}
+		matrix = new boolean[N][M];
+
+		// board 정의
+		boolean flag = true;
+		for (int i = 0; i < 8; i++) {
 			flag ^= true;
-		}
-		for(int i = 0; i < N; i++)
-		{
-			st = new StringTokenizer(br.readLine(), " ");
-			sb.append(st.nextToken());
-			for(int j = 0; j < M; j++)
-			{
-				if(sb.charAt(j) == 'B')
-				{
-					matrix[i][j] = 1;
-				}
-				else
-				{
-					matrix[i][j] = 0;
-				}
+			for (int j = 0; j < 8; j++) {
+				board[0][i][j] = flag;
+				flag ^= true;
+				board[1][i][j] = flag;
 			}
-			sb.setLength(0);
 		}
-		for(int i = 0; i < N; i++)
-		{
-			if(i+8 > N) continue;
-			for(int j = 0; j < M; j++)
-			{
-				if(j+8 > M) continue;
+
+		for (int i = 0; i < N; i++) {
+			StringBuilder sb = new StringBuilder(br.readLine());
+			for (int j = 0; j < M; j++) {
+				matrix[i][j] = (sb.charAt(j) == 'B') ? true : false;
+			}
+		}
+
+		for (int i = 0; i <= N - 8; i++) {
+			for (int j = 0; j <= M - 8; j++) {
 				solve(i, j);
 			}
 		}
