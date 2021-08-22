fda4013df06287cc307d1fa20d17c9dabf5ab138
kkupalro <dupalronet@hanmail.net>
2021-08-22 17:22:32 +0900

    0822|Programmers|LV1|x만큼 간격이 있는 n개의 숫자

--- Programmers/LV1/x만큼 간격이 있는 n개의 숫자.java
+++ Programmers/LV1/x만큼 간격이 있는 n개의 숫자.java
@@ -1,11 +1,11 @@
 class Solution {
-  public long[] solution(int x, int n) {
-      long[] answer = new long[n];
-      answer[0] = x;
-      for(int i = 1; i < n; i++)
-      {
-          answer[i] = answer[i-1] + x;
-      }
-      return answer;
-  }
+    public long[] solution(int x, int n) {
+        long[] answer = {};
+        answer = new long[n];
+        answer[0] = x;
+        for (int i = 1; i < answer.length; i++) {
+			answer[i] = answer[i-1] + x;
+		}
+        return answer;
+    }
 }
