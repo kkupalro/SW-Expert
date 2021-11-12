0e8eaa0c939c732949ceafd7ac4a3a104845cd39
kkupalro <dupalronet@hanmail.net>
2021-11-12 16:29:05 +0900

    1112|Programmers|LV1

--- Programmers/LV1/수박수박수박수박수박수.java
+++ Programmers/LV1/수박수박수박수박수박수.java
@@ -1,18 +1,14 @@
 class Solution {
-  public String solution(int n) {
-      String answer = "";
-      boolean check[] = new boolean[10001];
-      boolean flag = false;
-      for(int i = 0; i <= 10000; i++)
-      {
-          flag ^= true;
-          check[i] = flag;
+    public String solution(int n) {
+    String answer = "";
+    boolean flag = false;
+    while (n-- > 0) {
+      if (flag ^= true) {
+        answer += "수";
+      } else {
+        answer += "박";
       }
-      for(int i = 0; i < n; i++)
-      {
-          if(check[i]) answer += "수";
-          else answer += "박";
-      }
-      return answer;
+    }
+    return answer;
   }
 }
--- Programmers/LV1/시저 암호.java
+++ Programmers/LV1/시저 암호.java
@@ -1,25 +1,30 @@
 class Solution {
-  public String solution(String s, int n) {
-      String answer = "";
-      // A : 65 , Z : 90, A ~ Z : 25
-      // a : 97 , z :122. a ~ z : 25
-      for(int i = 0; i < s.length(); i++)
-      {
-          int num = (int)s.charAt(i);
-          if(num >= 65 && num <= 90) 
-          {
-              num += n;
-              num = (num>90)?num-26:num;
-              answer += (char)num;
+    public String solution(String s, int n) {
+    String answer = "";
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) == ' ') {
+        answer += " ";
+      } else {
+        int num = s.charAt(i);
+        if (num >= 65 && num <= 90) {
+          if (num + n > 90) {
+            num += n - 26;
+          } else {
+            num += n;
           }
-          else if(num >= 97 && num <= 122)
-          {
-              num += n;
-              num = (num>122)?num-26:num;
-              answer += (char)num;
+          answer += (char) num;
+
+        } else if (num >= 97 && num <= 122) {
+          if (num + n > 122) {
+            num += n - 26;
+          } else {
+            num += n;
           }
-          else answer += " ";
+          answer += (char) num;
+        }
       }
-      return answer;
+    }
+
+    return answer;
   }
 }
